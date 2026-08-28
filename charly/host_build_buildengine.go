5618a7bc65c78393f560176ffa8330c83e974186
atrawog <atrawog@users.noreply.github.com>
2026-08-28 21:47:28 +0200

    chore: note the B12 regression gate location

--- charly/host_build_buildengine.go
+++ charly/host_build_buildengine.go
@@ -104,3 +104,4 @@ var _ = func() bool {
 	registerHostBuilder("buildengine-context-ignore-baseline", typedHostBuilder("buildengine-context-ignore-baseline", hostBuildContextIgnoreBaseline))
 	return true
 }()
+// B12 regression gate: see host_build_buildengine_test.go
